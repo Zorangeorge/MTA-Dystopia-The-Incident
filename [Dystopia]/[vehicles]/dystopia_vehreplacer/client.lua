93186a78b9a929d319b3d5076f099dfb0cb34355
Zorgman <37583002+Zorangeorge@users.noreply.github.com>
2019-02-20 01:10:02 +0200

    #ADD
    -added Establishment R01 texture to FBI van

--- [Dystopia]/[vehicles]/dystopia_vehreplacer/client.lua
+++ [Dystopia]/[vehicles]/dystopia_vehreplacer/client.lua
@@ -2,3 +2,8 @@ txd = engineLoadTXD("skins/quad.txd")
 engineImportTXD(txd, 471)
 dff = engineLoadDFF("skins/quad.dff", 471)
 engineReplaceModel(dff, 471)
+
+txd = engineLoadTXD("skins/fbitruck.txd")
+engineImportTXD(txd, 528)
+dff = engineLoadDFF("skins/fbitruck.dff", 528)
+engineReplaceModel(dff, 528)
